cc1d3180a808620fcff5514420e8993142a5a337
praja4267 <praja4267@gmail.com>
2016-11-15 17:10:22 +0530

    Add files via upload

---
+++ GitRepoTest/GitRepoTest/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  GitRepoTest
+//
+//  Created by Active Mac05 on 15/11/16.
+//  Copyright © 2016 techactive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func GitButtonPressed(sender: AnyObject) {
+        print("Git button is pressed now....")
+    }
+    
+    
+
+}
+
